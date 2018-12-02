6bd0e2b33c6504a8e42391f42931ad1c4c13f53c
Drew Hanson <drew.o.hanson@gmail.com>
2018-12-01 22:25:31 -0700

    Adding assignment 1.4.js

---
+++ Wekk 1/Hanson1.5 .js
@@ -0,0 +1,44 @@
+var programHeader = {
+    firstName : "Drew",
+    lastName  : "Hanson",
+    assignment : "assignment 1.5",
+    Date  : "12/01/2018"
+};
+
+console.log(programHeader);
+
+var Employee1= {
+    firstName : "Steve",
+    lastName  : "Rogers",
+    PayRate : "10.5",
+    HireDate  : "10/01/2014"
+};
+var Employee2= {
+    firstName : "Tony",
+    lastName  : "Stark",
+    PayRate : "50.5",
+    HireDate  : "10/02/2014"
+};
+var Employee3= {
+    firstName : "Peter",
+    lastName  : "Parker",
+    PayRate : "8.5",
+    HireDate  : "10/09/2014"
+};
+var Employee4= {
+    firstName : "Bruce",
+    lastName  : "Banner",
+    PayRate : "15.8",
+    HireDate  : "10/14/2014"
+};
+var Employee5= {
+    firstName : "Stan",
+    lastName  : "Lee",
+    PayRate : "105.5",
+    HireDate  : "10/20/2014"
+};
+console.log(Employee1);
+console.log(Employee2);
+console.log(Employee3);
+console.log(Employee4);
+console.log(Employee5);
