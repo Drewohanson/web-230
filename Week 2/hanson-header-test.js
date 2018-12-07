f930afa7ce59c8a00c43a3d45bbdc5d63357f09f
Drew Hanson <drew.o.hanson@gmail.com>
2018-12-06 20:47:49 -0700

    adding assigment hanson-header-test.js

---
+++ Week 2/hanson-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Drew hanson
+; Date:   06 December 2018
+; Description: Displays a formatted header
+;===========================================
+*/
+
+var header = require('./header.js');
+
+console.log(header.display("Drew", "Hanson", "Week 2"));
---
+++ Week 2/hanson-header.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso 
+; Date:   04 December 2018
+; Modified By: Drew Hanson
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output 
+* Description: Returns a well-formatted string header
+*/
+exports.display = function (Drew, Hanson, Hanson-header.js) {
+	let output = '\n' + Drew + ' ' + Hanson + '\n' + Hanson-header.js + '\nDate: ' + 
+	new Date().toLocaleDateString()
+
+	return output  
+}
+
+const header = require(hanson-header.js);
+
+
+
