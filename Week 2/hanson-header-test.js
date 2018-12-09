2b2fc6ac417e782b140caeab4ed40b8ef7327702
Drew Hanson <drew.o.hanson@gmail.com>
2018-12-09 14:59:36 -0700

    adding assignment hanson-header-test.js

--- Week 2/hanson-header-test.js
+++ Week 2/hanson-header-test.js
@@ -7,6 +7,12 @@
 ;===========================================
 */
 
+<<<<<<< HEAD
 const header = require('C:\Users\Hanson\bu-webdev\web-231\Week 2\hanson-header.js');
+=======
+const header = require('./hanson.header.js');
 
-console.log(header.display("Drew", "Hanson", "Week 2"));
+console.log(header.display("Drew" , "Hanson" , "Week 2"));
+
+>>>>>>> 14151a723bb6adbc1ea9455b31acc0dc7b23afdd
+
