f6eb4de88b1cc41e982cc3aec13bf76469e0b9c7
Drew Hanson <drew.o.hanson@gmail.com>
2019-01-16 22:39:40 -0700

    adding assinment 6.3.js

---
+++ Week 6/6.3.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title:  Hanson-6.3.js
+; Author: Drew Hanson
+; Date:   16 January 2019
+; Modified By:
+; Description: 
+;===========================================
+
+*/
+
+var header = require('../hanson-header.js');
+
+// Output of header.display  that accepts first name, last name, assignment name
+console.log(header.display("Drew", "Hanson", "Exercise 6.3"));
+
+//object literal
+var ticketLog = {
+    id: 101,
+    name: "Help Desk Support",
+    requestor: "Bob Jones"
+  };
+  console.log(ticketLog);
